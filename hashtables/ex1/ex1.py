26288c013f880f84a2a0da2fdcbfd368a6cdf9fe
Michael-B1764 <mikebailar@gmail.com>
2020-08-07 11:01:28 -0700

    walked through 1 with TL. Will attempt to do 3 others.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,16 +3,28 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     weight_list_cache = {}
-    for i in range(length):
-        weight_list_cache[weights[i]] = i
-    print(weight_list_cache)
-    print(weights)
-    weight_counter = 0
-    for item in weight_list_cache.items():
 
-        print(limit, item[0], limit - weights[weight_counter], end=": ")
-        print(limit - weights[weight_counter])
-        weight_counter += 1
-    #print(weight_list_cache[15])
-    
+    for i in range(length):
+        comparison_value = limit - weights[i]
+        if comparison_value in weight_list_cache:
+            complement = weight_list_cache[comparison_value]
+            answer = (i, complement)
+            return answer
+
+        else:
+            weight_list_cache[weights[i]] = i
+
     return None
+    # for weight in weights:
+    #     weight_list_cache[weight] = limit - weight
+    # print(weight_list_cache)
+
+    # for weight in weights:
+    #     if weight_list_cache[weight] in weights:
+    #         answer = ( weights.index(limit - weight), weights.index(weight))
+    #         return answer
+
+if __name__ == '__main__':
+    weights_2 = [4, 4]
+    answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
+    print(answer_2)
