4c78ae53e6a520103bf33912c6d219eba0f63b6c
Michael-B1764 <mikebailar@gmail.com>
2020-08-07 10:11:54 -0700

    could not complete 1 thing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight_list_cache = {}
+    for i in range(length):
+        weight_list_cache[weights[i]] = i
+    print(weight_list_cache)
+    print(weights)
+    weight_counter = 0
+    for item in weight_list_cache.items():
 
+        print(limit, item[0], limit - weights[weight_counter], end=": ")
+        print(limit - weights[weight_counter])
+        weight_counter += 1
+    #print(weight_list_cache[15])
+    
     return None
